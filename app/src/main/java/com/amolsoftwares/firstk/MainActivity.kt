b5ef3e1b8037a6d3d698f2dfa9acec4b4e4a3cf3
Amol <amolgadge663@gmail.com>
2021-06-14 13:32:03 +0530

    Validating User input for email and password empty

--- app/src/main/java/com/amolsoftwares/firstk/MainActivity.kt
+++ app/src/main/java/com/amolsoftwares/firstk/MainActivity.kt
@@ -2,6 +2,7 @@ package com.amolsoftwares.firstk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -10,57 +11,67 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    /*lateinit var str_email: String
-    lateinit var str_pass: String*/
+    lateinit var email_txt: EditText
+    lateinit var pass_txt: EditText
+    lateinit var login_btn: Button
+
+    lateinit var email: String
+    lateinit var passw: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("TAG","onCreate is called")
+        Log.i("TAG", "onCreate is called")
         val makeText = Toast.makeText(this, "onCreate is called", Toast.LENGTH_SHORT)
         makeText.show()
 
-        /*val login_btn = findViewById<Button>(R.id.login_btn)
-        val email_txt = findViewById<EditText>(R.id.email).text.toString()
-        val pass_txt = findViewById<EditText>(R.id.pass).text.toString()*/
-
-        /*str_email = email_txt.text.toString()
-        str_pass = pass_txt.text.toString()*/
+        login_btn = findViewById(R.id.login_btn)
+        email_txt = findViewById<EditText>(R.id.email)
+        pass_txt = findViewById<EditText>(R.id.pass)
 
         login_btn.setOnClickListener {
-            if (email_txt.isBlank() && pass_txt.isBlank())
-                Toast.makeText(this, "Login Button Click", Toast.LENGTH_SHORT).show()
+
+            email = email_txt.text.toString()
+            passw = pass_txt.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Email should not be empty", Toast.LENGTH_SHORT).show()
+            } else if (passw.isEmpty()) {
+                Toast.makeText(this, "Password should not be empty", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i("TAG","onStart is called")
+        Log.i("TAG", "onStart is called")
         Toast.makeText(this, "onStart is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("TAG","onResume is called")
+        Log.i("TAG", "onResume is called")
         Toast.makeText(this, "onResume is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i("TAG","onPause is called")
+        Log.i("TAG", "onPause is called")
         Toast.makeText(this, "onPause is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("TAG","onPause is called")
+        Log.i("TAG", "onPause is called")
         Toast.makeText(this, "onPause is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("TAG","onPause is called")
+        Log.i("TAG", "onPause is called")
         Toast.makeText(this, "onPause is called", Toast.LENGTH_SHORT).show()
     }
 }
