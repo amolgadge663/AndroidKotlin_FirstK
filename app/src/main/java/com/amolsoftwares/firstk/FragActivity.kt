81db3299250b0574d6d64e1f5955b64ab3db9628
Amol <amolgadge663@gmail.com>
2021-06-15 10:04:19 +0530

    Switching fragments

--- app/src/main/java/com/amolsoftwares/firstk/FragActivity.kt
+++ app/src/main/java/com/amolsoftwares/firstk/FragActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class FragActivity : AppCompatActivity() {
 
@@ -21,14 +23,39 @@ class FragActivity : AppCompatActivity() {
 
         fr1.setOnClickListener {
             Toast.makeText(this, "frg 1", Toast.LENGTH_SHORT).show()
+
+            val f_frag = BlankFragment1()
+            val mgr = supportFragmentManager
+            val transaction = mgr.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, f_frag)
+            transaction.addToBackStack(null)
+            transaction.commit()
+
         }
 
         fr2.setOnClickListener {
             Toast.makeText(this, "frg 2", Toast.LENGTH_SHORT).show()
+
+            val s_frag = BlankFragment2()
+            val mgr = supportFragmentManager
+            val transaction = mgr.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, s_frag)
+            transaction.addToBackStack(null)
+            transaction.commit()
+
         }
 
         fr3.setOnClickListener {
             Toast.makeText(this, "frg 3", Toast.LENGTH_SHORT).show()
+
+            val t_frag = BlankFragment3()
+            val mgr = supportFragmentManager
+            val transaction = mgr.beginTransaction()
+            transaction.replace(R.id.fragmentContainerView, t_frag)
+            transaction.addToBackStack(null)
+            transaction.commit()
+
         }
     }
+
 }
